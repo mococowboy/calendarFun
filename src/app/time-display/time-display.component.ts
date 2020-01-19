a004eb9296e1755a859f224154b55ca80761ab94
mtolson <mtolson@live.com>
2020-01-18 19:15:28 -0500

    Automatically focus on the time field when a day is selected.

--- src/app/time-display/time-display.component.ts
+++ src/app/time-display/time-display.component.ts
@@ -1,4 +1,11 @@
-import {Component, Input, OnChanges, SimpleChanges} from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  Input,
+  OnChanges,
+  SimpleChanges,
+  ViewChild
+} from '@angular/core';
 import {MonthService} from "../month.service";
 import {FormControl} from "@angular/forms";
 
@@ -7,17 +14,25 @@ import {FormControl} from "@angular/forms";
   templateUrl: './time-display.component.html',
   styleUrls: ['./time-display.component.css']
 })
-export class TimeDisplayComponent implements OnChanges {
+export class TimeDisplayComponent implements OnChanges, AfterViewInit {
+
+
 
   @Input()
   date: Date;
   times: Date[] = [];
   monthService: MonthService;
   timeSelected: FormControl;
+  @ViewChild('selectElement', {static: false})
+  selectElement;
 
   constructor(monthService: MonthService) {
     this.monthService = monthService;
     this.timeSelected = new FormControl('');
+  }
+
+  ngAfterViewInit(): void {
+    this.selectElement.nativeElement.focus();
   }
 
   ngOnChanges(changes: SimpleChanges) {
